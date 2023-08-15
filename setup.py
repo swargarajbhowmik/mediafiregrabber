cfae62c44b6144f322a06a4434436a43193b94aa
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2023-08-15 18:18:40 +0530

    Added Build & Dist

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="MediaFireGrabber",
+    name="mediafiregrabber",
     version="1.0.0",
     packages=find_packages(),
     entry_points={
