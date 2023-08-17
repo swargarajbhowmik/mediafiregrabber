c04f5d5251c12df0b8dde248ea20b034cc297a15
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2023-08-17 09:21:51 +0530

    Fixed CLI Path Bug

--- mediafiregrabber/cli.py
+++ mediafiregrabber/cli.py
@@ -6,12 +6,15 @@ def download():
     parser.add_argument("url", nargs="+", help="<Mediafire URL>")
     parser.add_argument("--path", default=None, help="path to save downloaded file(s)")
     args = parser.parse_args()
-
+    if args.path:
+        save = args.path
+    else:
+        save = "./"
     for url in args.url:
         try:
-            mediafiregrabber.download(url, save=args.path)
-        except:
-            print(f"Failed to download from {url}.")
+            mediafiregrabber.download(url, save=save)
+        except Exception as e:
+            print(f"Failed to download from {e}.")
 
 if __name__ == "__main__":
     download()
